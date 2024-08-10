4086b4cbbcb9b490547f7cb05fa9f16acd4c70be
netanel azuz <netanelazuz@gmail.com>
2024-08-10 21:09:47 +0300

    staging start of project
    added required directories, libraries and modules to start

---
+++ app.py
@@ -0,0 +1,6 @@
+# this project goal is to give the user as much information on a stock he is interested in: 
+# such as: current value (live), latest news related, market value, buyers - sellers ratio, graph of current status
+# this will be done as the program takes a market ticker as an input and the rest is done by itself.
+# features for the future: *a few tabs to enable multiple stocks status. **buy/sell reccomendation by technical analysis.
+
+from flask import Flask
